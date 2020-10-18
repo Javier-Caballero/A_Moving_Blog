1844d4f25f8d8ce1ad0302be58d336fdb04cba59
Javier_Caballero <javier.caballero@mac.com>
2020-10-18 17:18:43 -0400

    add css for entry image and body

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
+let json;
+
 fetch("content.json")
   .then(response => response.json())
   .then(json => loadContent(json));
 
 function loadContent (json) {
 //  console.log(json);
+  
   let html = ""
   for (let i = 0; i < json.entry.length; i++) {
     let image = json.entry[i].image;
     let title = json.entry[i].title;
     let date = json.entry[i].date;
     let body = json.entry[i].content;
-    html += `<div class="post"><img src="${image}" class="post-img" alt="image" width="600" height="400"><div class="title">${title}</div><div class="date">${date}</div><div class="body">${body}</div></div>`;
+    html += `<div class="post"><div class="img-box"><img src="${image}" alt="image"></div><div class="title">${title}</div><div class="date">${date}</div><div class="body">${body}</div></div>`;
   }
   document.getElementById('content-container').innerHTML = html;
 }
